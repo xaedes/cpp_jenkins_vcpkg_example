51b8c3019a241cb57ce71e4eccc2a1efa07f3cdd
xaedes <xaedes@gmail.com>
2022-06-29 16:43:32 +0200

    update ci.bat

--- ci.bat
+++ ci.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem ---------------------------------------------------------------------------
 if [%~1]==[all] goto all
 if [%~1]==[clean] goto clean
 if [%~1]==[tools] goto tools
@@ -8,29 +9,25 @@ if [%~1]==[test] goto test
 if [%~1]==[help] goto help
 goto help
 
-
+rem ---------------------------------------------------------------------------
 :all
-
 %~0 clean && %~0 tools && %~0 build && %~0 test
-
-
 goto exit
 
+rem ---------------------------------------------------------------------------
 :clean
 echo Cleaning up...
-
 if exist "%~dp0\tools\" (
     rmdir /Q /S "%~dp0\tools\"
 )
 if exist "%~dp0\build\" (
     rmdir /Q /S "%~dp0\build\"
 )
-
 goto exit
 
+rem ---------------------------------------------------------------------------
 :tools
 echo Preparing tools...
-
 if not exist "%~dp0\tools\" (
     mkdir "%~dp0\tools\"
 )
@@ -38,13 +35,11 @@ if not exist "%~dp0\tools\vcpkg\vcpkg" (
     git clone https://github.com/microsoft/vcpkg.git "%~dp0\\tools\\vcpkg\\"
     "%~dp0\tools\vcpkg\bootstrap-vcpkg.bat" -disableMetrics
 )
-
-
 goto exit
 
+rem ---------------------------------------------------------------------------
 :build
 echo Building...
-
 rem set VCPKG_FEATURE_FLAGS=versions
 set BUILD_TYPE=Release
 set VCPKG_TARGET_TRIPLET=x64-windows
@@ -54,18 +49,18 @@ cmake --build %~dp0\build\Windows\Win64
 echo off
 goto exit
 
+rem ---------------------------------------------------------------------------
 :test
 echo Testing...
-
 echo on
 ctest --test-dir "%~dp0\build\Windows\Win64\example_tests\" 
 echo off
-
 goto exit
 
+rem ---------------------------------------------------------------------------
 :help
-
 echo Usage:
 echo %~0 [all^|clean^|tools^|build^|test^|help]
 
+rem ---------------------------------------------------------------------------
 :exit
